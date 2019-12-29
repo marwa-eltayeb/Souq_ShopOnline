598c1ecb424f0b2801d004cbf168fd13aa3240d9
Marwa-Said <marwa_eltayeb@yahoo.com>
2019-12-29 20:42:05 +0200

    Create Splash Screen

---
+++ Souq_Android/app/src/main/java/com/marwaeltayeb/souq/SplashActivity.java
@@ -0,0 +1,27 @@
+package com.marwaeltayeb.souq;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        int SPLASH_TIME_OUT = 2000;
+        new Handler().postDelayed(new Runnable() {
+
+            @Override
+            public void run() {
+                Intent i = new Intent(SplashActivity.this, MainActivity.class);
+                startActivity(i);
+
+                finish();
+            }
+        }, SPLASH_TIME_OUT);
+    }
+}
