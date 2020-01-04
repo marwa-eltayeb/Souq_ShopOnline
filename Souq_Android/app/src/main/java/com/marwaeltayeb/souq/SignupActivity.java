f845a5c9796ac9e802c3fc03f7fa48a657e11382
Marwa-Said <marwa_eltayeb@yahoo.com>
2020-01-04 22:55:28 +0200

    Set BoldStyle Programmatically

---
+++ Souq_Android/app/src/main/java/com/marwaeltayeb/souq/SignupActivity.java
@@ -0,0 +1,36 @@
+package com.marwaeltayeb.souq;
+
+import android.databinding.DataBindingUtil;
+import android.graphics.Typeface;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.text.Spannable;
+import android.text.SpannableString;
+import android.text.style.StyleSpan;
+
+import com.marwaeltayeb.souq.databinding.ActivitySignupBinding;
+
+public class SignupActivity extends AppCompatActivity {
+
+    private ActivitySignupBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_signup);
+
+        setBoldStyle();
+
+    }
+
+
+    private void setBoldStyle(){
+        String boldText =  getString(R.string.boldText);
+        String normalText = getString(R.string.normalText);
+        SpannableString str = new SpannableString(boldText + normalText);
+        str.setSpan(new StyleSpan(Typeface.BOLD), 0, boldText.length(), Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
+        binding.login.setText(str);
+    }
+
+
+}
