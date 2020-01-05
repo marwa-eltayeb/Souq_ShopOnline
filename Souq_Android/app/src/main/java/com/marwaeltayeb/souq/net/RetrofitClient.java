8e2653c3cf1f0ed599603521f430b106c3162bb2
Marwa-Said <marwa_eltayeb@yahoo.com>
2020-01-05 23:10:01 +0200

    Create RetrofitClient

---
+++ Souq_Android/app/src/main/java/com/marwaeltayeb/souq/net/RetrofitClient.java
@@ -0,0 +1,31 @@
+package com.marwaeltayeb.souq.net;
+
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+public class RetrofitClient {
+
+    private static final String BASE_URL = "http://192.168.1.4:3000/users/";
+    private static RetrofitClient mInstance;
+    private Retrofit retrofit;
+
+
+    private RetrofitClient() {
+        retrofit = new Retrofit.Builder()
+                .baseUrl(BASE_URL)
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+    }
+
+    public static synchronized RetrofitClient getInstance() {
+        if (mInstance == null) {
+            mInstance = new RetrofitClient();
+        }
+        return mInstance;
+    }
+
+    public UserApi getUserApi() {
+        return retrofit.create(UserApi.class);
+    }
+
+}
