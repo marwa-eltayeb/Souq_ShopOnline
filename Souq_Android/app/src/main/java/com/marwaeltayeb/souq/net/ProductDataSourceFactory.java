b90c173a62542d25b779f453cdd5488be828db35
Marwa-Said <marwa_eltayeb@yahoo.com>
2020-01-09 21:10:11 +0200

    Create ProductDataSourceFactory

--- Souq_Android/app/src/main/java/com/marwaeltayeb/souq/net/ProductDataSourceFactory.java
+++ Souq_Android/app/src/main/java/com/marwaeltayeb/souq/net/ProductDataSourceFactory.java
@@ -1,4 +1,33 @@
 package com.marwaeltayeb.souq.net;
 
-public class ProductDataSourceFactory {
+import android.arch.lifecycle.MutableLiveData;
+import android.arch.paging.DataSource;
+import android.arch.paging.PageKeyedDataSource;
+
+import com.marwaeltayeb.souq.model.ProductModel;
+
+public class ProductDataSourceFactory extends DataSource.Factory {
+
+    // Creating the mutable live database
+    private MutableLiveData<PageKeyedDataSource<Integer, ProductModel>> productLiveDataSource = new MutableLiveData<>();
+
+    public static ProductDataSource productDataSource;
+
+    @Override
+    public DataSource<Integer, ProductModel> create() {
+        // Getting our Data source object
+        productDataSource = new ProductDataSource();
+
+        // Posting the Data source to get the values
+        productLiveDataSource.postValue(productDataSource);
+
+        // Returning the Data source
+        return productDataSource;
+    }
+
+
+    // Getter for Product live DataSource
+    public MutableLiveData<PageKeyedDataSource<Integer, ProductModel>> getProductLiveDataSource() {
+        return productLiveDataSource;
+    }
 }
