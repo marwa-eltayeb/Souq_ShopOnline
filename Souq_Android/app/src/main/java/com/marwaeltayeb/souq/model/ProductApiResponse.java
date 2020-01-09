946c13d209b2005c8c308d0d5a02bef3394f3fd4
Marwa-Said <marwa_eltayeb@yahoo.com>
2020-01-09 21:11:13 +0200

    Add ProductApiResponse for getting products

--- Souq_Android/app/src/main/java/com/marwaeltayeb/souq/model/ProductApiResponse.java
+++ Souq_Android/app/src/main/java/com/marwaeltayeb/souq/model/ProductApiResponse.java
@@ -1,4 +1,16 @@
 package com.marwaeltayeb.souq.model;
 
+import java.util.List;
+
 public class ProductApiResponse {
+
+    private List<ProductModel> products;
+
+    public List<ProductModel> getProducts() {
+        return products;
+    }
+
+    public void setProducts(List<ProductModel> products) {
+        this.products = products;
+    }
 }
