9f60de22d469281eafb3f902f2b7c990ffccd8a4
Marwa-Said <marwa_eltayeb@yahoo.com>
2020-01-09 21:09:36 +0200

    Create ProductDataSource

--- Souq_Android/app/src/main/java/com/marwaeltayeb/souq/net/ProductDataSource.java
+++ Souq_Android/app/src/main/java/com/marwaeltayeb/souq/net/ProductDataSource.java
@@ -1,4 +1,69 @@
 package com.marwaeltayeb.souq.net;
 
-public class ProductDataSource {
+import android.arch.paging.PageKeyedDataSource;
+import android.support.annotation.NonNull;
+
+import com.marwaeltayeb.souq.model.ProductApiResponse;
+import com.marwaeltayeb.souq.model.ProductModel;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
+public class ProductDataSource extends PageKeyedDataSource<Integer, ProductModel> {
+
+    private static final int FIRST_PAGE = 1;
+
+    static final int PAGE_SIZE = 20;
+
+    @Override
+    public void loadInitial(@NonNull LoadInitialParams<Integer> params, @NonNull LoadInitialCallback<Integer, ProductModel> callback) {
+        RetrofitClient.getInstance()
+                .getApi().getProductByCategory()
+                .enqueue(new Callback<ProductApiResponse>() {
+                    @Override
+                    public void onResponse(Call<ProductApiResponse> call, Response<ProductApiResponse> response) {
+
+                    }
+
+                    @Override
+                    public void onFailure(Call<ProductApiResponse> call, Throwable t) {
+
+                    }
+                });
+    }
+
+    @Override
+    public void loadBefore(@NonNull LoadParams<Integer> params, @NonNull LoadCallback<Integer, ProductModel> callback) {
+        RetrofitClient.getInstance()
+                .getApi().getProductByCategory()
+                .enqueue(new Callback<ProductApiResponse>() {
+                    @Override
+                    public void onResponse(Call<ProductApiResponse> call, Response<ProductApiResponse> response) {
+
+                    }
+
+                    @Override
+                    public void onFailure(Call<ProductApiResponse> call, Throwable t) {
+
+                    }
+                });
+    }
+
+    @Override
+    public void loadAfter(@NonNull LoadParams<Integer> params, @NonNull LoadCallback<Integer, ProductModel> callback) {
+        RetrofitClient.getInstance()
+                .getApi().getProductByCategory()
+                .enqueue(new Callback<ProductApiResponse>() {
+                    @Override
+                    public void onResponse(Call<ProductApiResponse> call, Response<ProductApiResponse> response) {
+
+                    }
+
+                    @Override
+                    public void onFailure(Call<ProductApiResponse> call, Throwable t) {
+
+                    }
+                });
+    }
 }
