aa00a7f4ef744c7227f693a8cda3b9e2b0b25a99
Marwa-Said <marwa_eltayeb@yahoo.com>
2020-01-12 23:24:06 +0200

    Add @SerializedName Annotation

--- Souq_Android/app/src/main/java/com/marwaeltayeb/souq/model/ProductApiResponse.java
+++ Souq_Android/app/src/main/java/com/marwaeltayeb/souq/model/ProductApiResponse.java
@@ -1,9 +1,12 @@
 package com.marwaeltayeb.souq.model;
 
+import com.google.gson.annotations.SerializedName;
+
 import java.util.List;
 
 public class ProductApiResponse {
 
+    @SerializedName("products")
     private List<Product> products;
 
     public List<Product> getProducts() {
