88e4c0bfe2678a052d956fc5017ec2934ddcb613
Marwa-Said <marwa_eltayeb@yahoo.com>
2020-01-13 01:07:15 +0200

    Display All Mobiles

---
+++ Souq_Android/app/src/main/java/com/marwaeltayeb/souq/ui/AllMobilesActivity.java
@@ -0,0 +1,55 @@
+package com.marwaeltayeb.souq.ui;
+
+import android.arch.lifecycle.Observer;
+import android.arch.lifecycle.ViewModelProviders;
+import android.arch.paging.PagedList;
+import android.databinding.DataBindingUtil;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+
+import com.marwaeltayeb.souq.R;
+import com.marwaeltayeb.souq.ViewModel.ProductViewModel;
+import com.marwaeltayeb.souq.adapter.ProductAdapter;
+import com.marwaeltayeb.souq.databinding.ActivityAllMobilesBinding;
+import com.marwaeltayeb.souq.model.Product;
+
+public class AllMobilesActivity extends AppCompatActivity {
+
+    private ActivityAllMobilesBinding binding;
+    private ProductAdapter productAdapter;
+    private ProductViewModel productViewModel;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_all_mobiles);
+
+        productViewModel = ViewModelProviders.of(this).get(ProductViewModel.class);
+
+        setupRecyclerViews();
+
+        getAllMobiles();
+    }
+
+    private void setupRecyclerViews() {
+        // Mobiles
+        binding.allMobilesRecyclerView.setLayoutManager(new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false));
+        binding.allMobilesRecyclerView.setHasFixedSize(true);
+        productAdapter = new ProductAdapter(this);
+    }
+
+    public void getAllMobiles() {
+        // Observe the productPagedList from ViewModel
+        productViewModel.productPagedList.observe(this, new Observer<PagedList<Product>>() {
+            @Override
+            public void onChanged(@Nullable PagedList<Product> products) {
+                productAdapter.notifyDataSetChanged();
+                productAdapter.submitList(products);
+            }
+        });
+
+        binding.allMobilesRecyclerView.setAdapter(productAdapter);
+    }
+}
