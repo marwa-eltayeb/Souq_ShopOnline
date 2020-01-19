6631afd351ec4bd32ad5ef2ead9565fa305bc363
Marwa-Said <marwa_eltayeb@yahoo.com>
2020-01-20 00:13:49 +0200

    Save User Data

---
+++ Souq_Android/app/src/main/java/com/marwaeltayeb/souq/storage/SharedPrefManager.java
@@ -0,0 +1,60 @@
+package com.marwaeltayeb.souq.storage;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+
+import com.marwaeltayeb.souq.model.LoginApiResponse;
+
+public class SharedPrefManager {
+
+    private static final String SHARED_PREF_NAME = "shared_preference";
+
+    private static SharedPrefManager mInstance;
+    private Context mCtx;
+
+    private SharedPrefManager(Context mCtx) {
+        this.mCtx = mCtx;
+    }
+
+
+    public static synchronized SharedPrefManager getInstance(Context mCtx) {
+        if (mInstance == null) {
+            mInstance = new SharedPrefManager(mCtx);
+        }
+        return mInstance;
+    }
+
+
+    public void saveUserInfo(int id, String token) {
+
+        SharedPreferences sharedPreferences = mCtx.getSharedPreferences(SHARED_PREF_NAME, Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+
+        editor.putInt("id", id);
+        editor.putString("token", token);
+        editor.apply();
+    }
+
+
+    public boolean isLoggedIn() {
+        SharedPreferences sharedPreferences = mCtx.getSharedPreferences(SHARED_PREF_NAME, Context.MODE_PRIVATE);
+        return sharedPreferences.getInt("id", -1) != -1;
+    }
+
+
+    public LoginApiResponse getUserInfo() {
+        SharedPreferences sharedPreferences = mCtx.getSharedPreferences(SHARED_PREF_NAME, Context.MODE_PRIVATE);
+        return new LoginApiResponse(
+                sharedPreferences.getInt("name", -1),
+                sharedPreferences.getString("token", null)
+        );
+    }
+
+    public void clear() {
+        SharedPreferences sharedPreferences = mCtx.getSharedPreferences(SHARED_PREF_NAME, Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        editor.clear();
+        editor.apply();
+    }
+
+}
