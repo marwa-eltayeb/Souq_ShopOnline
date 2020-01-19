2c2e1ed62166a07df315ddd37d9e0541b3606ac3
Marwa-Said <marwa_eltayeb@yahoo.com>
2020-01-20 00:28:15 +0200

    Modify RegisterApiResponse

--- Souq_Android/app/src/main/java/com/marwaeltayeb/souq/model/RegisterApiResponse.java
+++ Souq_Android/app/src/main/java/com/marwaeltayeb/souq/model/RegisterApiResponse.java
@@ -2,12 +2,17 @@ package com.marwaeltayeb.souq.model;
 
 public class RegisterApiResponse {
 
+    private int id;
     private boolean error;
     private String message;
 
     public RegisterApiResponse(boolean error, String message) {
         this.error = error;
         this.message = message;
+    }
+
+    public int getId() {
+        return id;
     }
 
     public boolean isError() {
