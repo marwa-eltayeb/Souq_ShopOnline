2f7dc7c6ad137f402b4778bc39f39f35cfcffbf6
Marwa-Said <marwa_eltayeb@yahoo.com>
2020-01-25 18:05:19 +0200

    Use LiveData with laptop lists

--- Souq_Android/app/src/main/java/com/marwaeltayeb/souq/ViewModel/ProductViewModel.java
+++ Souq_Android/app/src/main/java/com/marwaeltayeb/souq/ViewModel/ProductViewModel.java
@@ -7,40 +7,56 @@ import android.arch.paging.PageKeyedDataSource;
 import android.arch.paging.PagedList;
 
 import com.marwaeltayeb.souq.model.Product;
+import com.marwaeltayeb.souq.net.LaptopDataSourceFactory;
 import com.marwaeltayeb.souq.net.ProductDataSource;
 import com.marwaeltayeb.souq.net.ProductDataSourceFactory;
-
-import static com.marwaeltayeb.souq.net.ProductDataSourceFactory.productDataSource;
 
 
 public class ProductViewModel extends ViewModel {
 
     // Create liveData for PagedList and PagedKeyedDataSource
     public LiveData<PagedList<Product>> productPagedList;
-    private LiveData<PageKeyedDataSource<Integer, Product>> liveDataSource;
+    private LiveData<PageKeyedDataSource<Integer, Product>> productLiveDataSource;
+
+    public LiveData<PagedList<Product>> laptopPagedList;
+    private LiveData<PageKeyedDataSource<Integer, Product>> laptopLiveDataSource;
+
+    private PagedList.Config pagedListConfig;
 
     // Constructor
     public ProductViewModel() {
-
-        // Get our database source factory
-        ProductDataSourceFactory productDataSourceFactory = new ProductDataSourceFactory();
-
-        // Get the live database source from database source factory
-        liveDataSource = productDataSourceFactory.getProductLiveDataSource();
-
         // Get PagedList configuration
-        PagedList.Config pagedListConfig =
+        pagedListConfig =
                 (new PagedList.Config.Builder())
                         .setEnablePlaceholders(false)
                         .setPageSize(ProductDataSource.PAGE_SIZE)
                         .build();
 
+        initProductDataSource();
+        initLaptopDataSource();
+    }
+
+    private void initProductDataSource(){
+        // Get our database source factory
+        ProductDataSourceFactory productDataSourceFactory = new ProductDataSourceFactory();
+
+        // Get the live database source from database source factory
+        productLiveDataSource = productDataSourceFactory.getProductLiveDataSource();
+
         // Build the paged list
         productPagedList = (new LivePagedListBuilder(productDataSourceFactory, pagedListConfig)).build();
     }
 
-    public void clear() {
-        productDataSource.invalidate();
+    private void initLaptopDataSource(){
+        // Get our database source factory
+        LaptopDataSourceFactory laptopDataSourceFactory = new LaptopDataSourceFactory();
+
+        // Get the live database source from database source factory
+        laptopLiveDataSource = laptopDataSourceFactory.getLaptopLiveDataSource();
+
+
+        // Build the paged list
+        laptopPagedList = (new LivePagedListBuilder(laptopDataSourceFactory, pagedListConfig)).build();
     }
 
 }
