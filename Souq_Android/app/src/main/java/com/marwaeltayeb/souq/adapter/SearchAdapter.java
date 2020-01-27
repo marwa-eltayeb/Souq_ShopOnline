fb1a0547cbd9faee32c92516ed4bcfdffbec8239
Marwa-Said <marwa_eltayeb@yahoo.com>
2020-01-27 23:40:30 +0200

    Create SearchAdapter

---
+++ Souq_Android/app/src/main/java/com/marwaeltayeb/souq/adapter/SearchAdapter.java
@@ -0,0 +1,99 @@
+package com.marwaeltayeb.souq.adapter;
+
+import android.content.Context;
+import android.databinding.DataBindingUtil;
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.bumptech.glide.Glide;
+import com.marwaeltayeb.souq.R;
+import com.marwaeltayeb.souq.databinding.SearchListItemBinding;
+import com.marwaeltayeb.souq.model.Product;
+
+import java.util.List;
+
+import static com.marwaeltayeb.souq.utils.Constant.LOCALHOST;
+
+public class SearchAdapter extends RecyclerView.Adapter<SearchAdapter.SearchViewHolder>{
+
+    private Context mContext;
+    // Declare an arrayList for products
+    private List<Product> productList;
+
+    private Product currentProduct;
+
+    // Create a final private SearchAdapterOnClickHandler called mClickHandler
+    private SearchAdapterOnClickHandler clickHandler;
+
+    /**
+     * The interface that receives onClick messages.
+     */
+    public interface SearchAdapterOnClickHandler {
+        void onClick(Product product);
+    }
+
+    public SearchAdapter(Context mContext,List<Product> productList,SearchAdapterOnClickHandler clickHandler) {
+        this.mContext = mContext;
+        this.productList = productList;
+        this.clickHandler = clickHandler;
+    }
+
+    @NonNull
+    @Override
+    public SearchViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int i) {
+        SearchListItemBinding searchListItemBinding = DataBindingUtil.inflate(LayoutInflater.from(parent.getContext()),R.layout.search_list_item,parent,false);
+        return new SearchViewHolder(searchListItemBinding);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull SearchViewHolder holder, int position) {
+        currentProduct = productList.get(position);
+        holder.binding.txtProductName.setText(currentProduct.getProductName());
+        holder.binding.txtProductPrice.setText(String.valueOf(currentProduct.getProductPrice()));
+
+        // Load the Product image into ImageView
+        String imageUrl = LOCALHOST + currentProduct.getProductImage().replaceAll("\\\\", "/");
+        Glide.with(mContext)
+                .load(imageUrl)
+                .into(holder.binding.imgProductImage);
+    }
+
+    @Override
+    public int getItemCount() {
+        if (productList == null) {
+            return 0;
+        }
+        return productList.size();
+    }
+
+    public void clear() {
+        int size = productList.size();
+        productList.clear();
+        notifyItemRangeRemoved(0, size);
+    }
+
+    class SearchViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener{
+
+        // Create view instances
+        private final SearchListItemBinding binding;
+
+        private SearchViewHolder(SearchListItemBinding binding) {
+            super(binding.getRoot());
+            this.binding = binding;
+            // Register a callback to be invoked when this view is clicked.
+            itemView.setOnClickListener(this);
+        }
+
+        @Override
+        public void onClick(View v) {
+            int position = getAdapterPosition();
+            // Get position of the product
+            currentProduct = productList.get(position);
+            // Send product through click
+            clickHandler.onClick(currentProduct);
+        }
+    }
+}
