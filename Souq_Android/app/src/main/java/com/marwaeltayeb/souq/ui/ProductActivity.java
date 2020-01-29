6ea4d20ece0f7770e3df1c6022e3f1e4c1e8ab69
Marwa-Said <marwa_eltayeb@yahoo.com>
2020-01-29 22:07:18 +0200

    Search for Products in Database

--- Souq_Android/app/src/main/java/com/marwaeltayeb/souq/ui/ProductActivity.java
+++ Souq_Android/app/src/main/java/com/marwaeltayeb/souq/ui/ProductActivity.java
@@ -5,6 +5,7 @@ import android.arch.lifecycle.ViewModelProviders;
 import android.arch.paging.PagedList;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.content.res.Configuration;
 import android.databinding.DataBindingUtil;
 import android.net.ConnectivityManager;
 import android.os.Build;
@@ -13,20 +14,34 @@ import android.support.annotation.Nullable;
 import android.support.design.widget.Snackbar;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.LinearLayoutManager;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.SearchView;
+import android.widget.Toast;
 
 import com.marwaeltayeb.souq.R;
 import com.marwaeltayeb.souq.ViewModel.ProductViewModel;
 import com.marwaeltayeb.souq.adapter.ProductAdapter;
+import com.marwaeltayeb.souq.adapter.SearchAdapter;
 import com.marwaeltayeb.souq.databinding.ActivityProductBinding;
 import com.marwaeltayeb.souq.model.Product;
+import com.marwaeltayeb.souq.model.ProductApiResponse;
+import com.marwaeltayeb.souq.net.RetrofitClient;
 import com.marwaeltayeb.souq.receiver.NetworkChangeReceiver;
 import com.marwaeltayeb.souq.utils.OnNetworkListener;
 import com.marwaeltayeb.souq.utils.Slide;
 
 import java.util.ArrayList;
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 import static com.marwaeltayeb.souq.utils.Constant.PRODUCT;
 import static com.marwaeltayeb.souq.utils.InternetUtils.isNetworkConnected;
@@ -37,6 +52,8 @@ public class ProductActivity extends AppCompatActivity implements View.OnClickLi
 
     private ProductAdapter mobileAdapter;
     private ProductAdapter laptopAdapter;
+    private SearchAdapter searchAdapter;
+    private List<Product> searchedMovieList;
 
     private ProductViewModel productViewModel;
 
@@ -75,8 +92,8 @@ public class ProductActivity extends AppCompatActivity implements View.OnClickLi
         binding.listOfLaptops.setHasFixedSize(true);
         binding.listOfLaptops.setLayoutManager(new LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false));
 
-        mobileAdapter = new ProductAdapter(this,this);
-        laptopAdapter = new ProductAdapter(this,this);;
+        mobileAdapter = new ProductAdapter(this, this);
+        laptopAdapter = new ProductAdapter(this, this);
     }
 
     private void getMobiles() {
@@ -91,7 +108,7 @@ public class ProductActivity extends AppCompatActivity implements View.OnClickLi
 
             binding.listOfMobiles.setAdapter(mobileAdapter);
             mobileAdapter.notifyDataSetChanged();
-        }else {
+        } else {
             showOrHideViews(View.INVISIBLE);
             showSnackBar();
         }
@@ -109,7 +126,7 @@ public class ProductActivity extends AppCompatActivity implements View.OnClickLi
 
             binding.listOfLaptops.setAdapter(laptopAdapter);
             laptopAdapter.notifyDataSetChanged();
-        }else {
+        } else {
             showOrHideViews(View.INVISIBLE);
             showSnackBar();
         }
@@ -204,10 +221,102 @@ public class ProductActivity extends AppCompatActivity implements View.OnClickLi
         startActivity(intent);
     }
 
-    private void showOrHideViews(int view){
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.search, menu);
+        MenuItem searchViewItem = menu.findItem
+                (R.id.action_search);
+        SearchView searchView = (SearchView)
+                searchViewItem.getActionView();
+
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                if (isNetworkConnected(getApplicationContext())) {
+                    Search(query);
+                }
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                if (isNetworkConnected(getApplicationContext())) {
+                    Search(newText);
+                }
+                return false;
+            }
+        });
+
+        searchView.setOnCloseListener(new SearchView.OnCloseListener() {
+            @Override
+            public boolean onClose() {
+                if (searchedMovieList != null) {
+                    searchAdapter.clear();
+                    setUpViews();
+                    showOrHideViews(View.VISIBLE);
+                    hideViews(View.VISIBLE);
+                    getMobiles();
+                    getLaptops();
+                }
+                return false;
+            }
+        });
+
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    private void setViewsAfterSearch() {
+        binding.listOfMobiles.setHasFixedSize(true);
+        binding.listOfMobiles.setLayoutManager(new GridLayoutManager(this, (getResources().getConfiguration().orientation == Configuration.ORIENTATION_PORTRAIT) ? 2 : 4));
+        showOrHideViews(View.GONE);
+        hideViews(View.GONE);
+    }
+
+    private void showOrHideViews(int view) {
         binding.textViewMobiles.setVisibility(view);
         binding.txtSeeAllMobiles.setVisibility(view);
         binding.textViewLaptops.setVisibility(view);
         binding.txtSeeAllLaptops.setVisibility(view);
     }
+
+    private void hideViews(int view) {
+        binding.listOfLaptops.setVisibility(view);
+        binding.imageSlider.setVisibility(view);
+    }
+
+    private void Search(String query) {
+        setViewsAfterSearch();
+
+        RetrofitClient.getInstance()
+                .getApi().searchForProduct(query)
+                .enqueue(new Callback<ProductApiResponse>() {
+                    @Override
+                    public void onResponse(Call<ProductApiResponse> call, Response<ProductApiResponse> response) {
+                        if (response.body() != null) {
+                            searchedMovieList = response.body().getProducts();
+                            if (searchedMovieList.isEmpty()) {
+                                Toast.makeText(ProductActivity.this, "No Result", Toast.LENGTH_SHORT).show();
+                            }
+
+                            searchAdapter = new SearchAdapter(getApplicationContext(), searchedMovieList, new SearchAdapter.SearchAdapterOnClickHandler() {
+                                @Override
+                                public void onClick(Product product) {
+                                    Intent intent = new Intent(ProductActivity.this, DetailsActivity.class);
+                                    // Pass an object of product class
+                                    intent.putExtra(PRODUCT, (product));
+                                    startActivity(intent);
+                                }
+                            });
+                        }
+                        binding.listOfMobiles.setAdapter(searchAdapter);
+                    }
+
+                    @Override
+                    public void onFailure(Call<ProductApiResponse> call, Throwable t) {
+                        Log.v("onFailure", " Failed to get products");
+                    }
+                });
+    }
+
+
 }
