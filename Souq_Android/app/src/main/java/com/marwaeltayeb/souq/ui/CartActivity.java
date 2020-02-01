468f89e79874efcd7ee2c89649f36f422be81d4e
Marwa-Said <marwa_eltayeb@yahoo.com>
2020-02-01 21:11:58 +0200

    Create Cart Activity

---
+++ Souq_Android/app/src/main/java/com/marwaeltayeb/souq/ui/CartActivity.java
@@ -0,0 +1,15 @@
+package com.marwaeltayeb.souq.ui;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.marwaeltayeb.souq.R;
+
+public class CartActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cart);
+    }
+}
