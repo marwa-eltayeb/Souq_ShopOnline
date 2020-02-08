b8906749741a212f079f56397653f0ac309b10cd
Marwa-Said <marwa_eltayeb@yahoo.com>
2020-02-08 22:00:57 +0200

    Get All favorite products

--- Souq_Backend/api/routes/favorites.js
+++ Souq_Backend/api/routes/favorites.js
@@ -4,6 +4,38 @@ const router = express.Router()
 // import file
 const database = require("../../config")
 
+// Get All favorite products
+router.get("/", (request, response) => {
+    var userId = request.query.userId;
+    var page = request.query.page;
+    var page_size = request.query.page_size;
+
+    console.log(typeof page);
+
+    if(page == null){
+        page = 0;
+     }
+ 
+     if(page_size == null){
+        page_size = 25;
+     }
+
+     const args = [
+        userId,
+        parseInt(page_size),
+        parseInt(page)
+    ];
+
+    const query = "SELECT product.name, product.price, product.image, product.category, product.quantity, product.supplier FROM favorite JOIN product JOIN User ON favorite.product_id = product.id AND favorite.user_id = user.id WHERE user_id = ? LIMIT ? OFFSET ?"
+    database.query(query, args, (error, result) => {
+        if(error) throw error;
+        response.status(200).json({
+            "favorites" : result
+        })
+
+    })
+});
+
 // Add Favorite Product
 router.post("/add", (request, response) => {
     const userId = request.body.userId
