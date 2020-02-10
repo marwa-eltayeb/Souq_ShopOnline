dbbd04a7ddb663f523142228e231dca18c7e3c06
Marwa-Said <marwa_eltayeb@yahoo.com>
2020-02-11 00:51:12 +0200

    Clear only id when user signs out

--- Souq_Android/app/src/main/java/com/marwaeltayeb/souq/storage/SharedPrefManager.java
+++ Souq_Android/app/src/main/java/com/marwaeltayeb/souq/storage/SharedPrefManager.java
@@ -62,7 +62,7 @@ public class SharedPrefManager {
     public void clear() {
         SharedPreferences sharedPreferences = mCtx.getSharedPreferences(SHARED_PREF_NAME, Context.MODE_PRIVATE);
         SharedPreferences.Editor editor = sharedPreferences.edit();
-        editor.clear();
+        editor.remove("id").apply();
         editor.apply();
     }
 
