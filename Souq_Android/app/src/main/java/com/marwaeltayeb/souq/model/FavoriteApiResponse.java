cd8f2a70e8b5f5b5776b8c2f1366ba19d88695b1
Marwa-Said <marwa_eltayeb@yahoo.com>
2020-02-16 01:01:46 +0200

    Create FavoriteApiResponse

---
+++ Souq_Android/app/src/main/java/com/marwaeltayeb/souq/model/FavoriteApiResponse.java
@@ -0,0 +1,15 @@
+package com.marwaeltayeb.souq.model;
+
+public class FavoriteApiResponse {
+
+    private int id;
+    private String message;
+
+    public int getId() {
+        return id;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+}
