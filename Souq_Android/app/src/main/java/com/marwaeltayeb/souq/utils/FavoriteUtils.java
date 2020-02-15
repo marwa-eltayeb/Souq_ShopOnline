a0db467c194a86fbe0080ec9e0f82397b629930d
Marwa-Said <marwa_eltayeb@yahoo.com>
2020-02-16 00:33:04 +0200

    Set Favorite State

---
+++ Souq_Android/app/src/main/java/com/marwaeltayeb/souq/utils/FavoriteUtils.java
@@ -0,0 +1,23 @@
+package com.marwaeltayeb.souq.utils;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+
+public class FavoriteUtils {
+
+    public static void setFavoriteState(Context context, int productId , boolean isFavourite){
+        SharedPreferences sharedpreferences = context.getSharedPreferences("favorite_data", Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedpreferences.edit();
+        editor.putBoolean(String.valueOf(productId), isFavourite);
+        editor.apply();
+    }
+
+    public static boolean getFavoriteState(Context context, int productId){
+        SharedPreferences sharedpreferences = context.getSharedPreferences("favorite_data", Context.MODE_PRIVATE);
+        return sharedpreferences.getBoolean(String.valueOf(productId), false);
+    }
+
+    public static void clearSharedPreferences(Context context){
+        context.getSharedPreferences("favorite_data", Context.MODE_PRIVATE).edit().clear().apply();
+    }
+}
