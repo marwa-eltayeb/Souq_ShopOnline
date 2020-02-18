3fc1bd11579540f5a19f8a6b8334150640a7f83b
Marwa-Said <marwa_eltayeb@yahoo.com>
2020-02-18 03:34:46 +0200

    Create AddProductActivity

---
+++ Souq_Android/app/src/main/java/com/marwaeltayeb/souq/view/AddProductActivity.java
@@ -0,0 +1,15 @@
+package com.marwaeltayeb.souq.view;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.marwaeltayeb.souq.R;
+
+public class AddProductActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_product);
+    }
+}
