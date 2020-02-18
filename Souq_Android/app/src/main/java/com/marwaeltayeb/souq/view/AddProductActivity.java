ae70fed221fb40b3e3cd3916d46236bc211556d3
Marwa-Said <marwa_eltayeb@yahoo.com>
2020-02-18 03:49:18 +0200

    Create menu Item for saving product

--- Souq_Android/app/src/main/java/com/marwaeltayeb/souq/view/AddProductActivity.java
+++ Souq_Android/app/src/main/java/com/marwaeltayeb/souq/view/AddProductActivity.java
@@ -2,6 +2,9 @@ package com.marwaeltayeb.souq.view;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 import com.marwaeltayeb.souq.R;
 
@@ -11,5 +14,24 @@ public class AddProductActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_product);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.add, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == R.id.action_save) {
+            addProduct();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void addProduct() {
+        Toast.makeText(this, "Save", Toast.LENGTH_SHORT).show();
     }
 }
