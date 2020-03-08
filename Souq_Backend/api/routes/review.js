189041a4e4e5749596bc7b02539de1568943099b
Marwa-Said <marwa_eltayeb@yahoo.com>
2020-03-09 01:04:41 +0200

    Add rate column

--- Souq_Backend/api/routes/review.js
+++ Souq_Backend/api/routes/review.js
@@ -4,16 +4,16 @@ const router = express.Router()
 // import file
 const database = require("../../config")
 
-
 // Add Review about Product
 router.post("/add", (request, response) => {
     const userId = request.body.userId
     const productId = request.body.productId
     const feedback = request.body.feedback
+    const rate = request.body.rate
 
-    const query = "INSERT INTO review(user_Id, product_Id, feedback, review_date) VALUES(?, ?, ?, NOW())"
+    const query = "INSERT INTO review(user_Id, product_Id, feedback, rate ,review_date) VALUES(?, ?, ?, ?,NOW())"
    
-    const args = [userId, productId, feedback]
+    const args = [userId, productId, feedback,rate]
 
     database.query(query, args, (error, result) => {
         if(error) throw error
