cb0eb98f8f5d1e02e59ec360e41c0e82b78b6d8f
Marwa-Said <marwa_eltayeb@yahoo.com>
2020-03-29 22:04:14 +0200

    Add ProgressBar and EmptyTextView to favorites

--- Souq_Android/app/src/main/java/com/marwaeltayeb/souq/view/WishListActivity.java
+++ Souq_Android/app/src/main/java/com/marwaeltayeb/souq/view/WishListActivity.java
@@ -6,6 +6,7 @@ import android.databinding.DataBindingUtil;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
+import android.view.View;
 
 import com.marwaeltayeb.souq.R;
 import com.marwaeltayeb.souq.ViewModel.FavoriteViewModel;
@@ -17,6 +18,7 @@ import com.marwaeltayeb.souq.storage.LoginUtils;
 import java.util.List;
 
 import static com.marwaeltayeb.souq.utils.Constant.PRODUCT;
+import static com.marwaeltayeb.souq.utils.InternetUtils.isNetworkConnected;
 
 public class WishListActivity extends AppCompatActivity {
 
@@ -35,26 +37,39 @@ public class WishListActivity extends AppCompatActivity {
         getFavorites();
     }
 
-    private void setUpRecyclerView(){
+    private void setUpRecyclerView() {
         binding.favoriteList.setLayoutManager(new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false));
         binding.favoriteList.setHasFixedSize(true);
         favoriteViewModel = ViewModelProviders.of(this).get(FavoriteViewModel.class);
     }
 
     private void getFavorites() {
-        favoriteViewModel.getFavorites(LoginUtils.getInstance(this).getUserInfo().getId()).observe(this, favoriteApiResponse -> {
-            if (favoriteApiResponse != null) {
-                favoriteList = favoriteApiResponse.getFavorites();
-                wishListAdapter = new WishListAdapter(getApplicationContext(), favoriteList, product -> {
-                    Intent intent = new Intent(WishListActivity.this, DetailsActivity.class);
-                    // Pass an object of product class
-                    intent.putExtra(PRODUCT, (product));
-                    startActivity(intent);
-                });
-            }
-
-            binding.favoriteList.setAdapter(wishListAdapter);
-            wishListAdapter.notifyDataSetChanged();
-        });
+        if (isNetworkConnected(this)) {
+            favoriteViewModel.getFavorites(LoginUtils.getInstance(this).getUserInfo().getId()).observe(this, favoriteApiResponse -> {
+                if (favoriteApiResponse != null) {
+                    favoriteList = favoriteApiResponse.getFavorites();
+                    if (favoriteList.size() == 0) {
+                        binding.noBookmarks.setVisibility(View.VISIBLE);
+                        binding.emptyWishlist.setVisibility(View.VISIBLE);
+                    }else {
+                        binding.favoriteList.setVisibility(View.VISIBLE);
+                    }
+                    wishListAdapter = new WishListAdapter(getApplicationContext(), favoriteList, product -> {
+                        Intent intent = new Intent(WishListActivity.this, DetailsActivity.class);
+                        // Pass an object of product class
+                        intent.putExtra(PRODUCT, (product));
+                        startActivity(intent);
+                    });
+                }
+                binding.loadingIndicator.setVisibility(View.GONE);
+                binding.favoriteList.setAdapter(wishListAdapter);
+                wishListAdapter.notifyDataSetChanged();
+            });
+        }else {
+            binding.emptyWishlist.setVisibility(View.VISIBLE);
+            binding.loadingIndicator.setVisibility(View.GONE);
+            binding.emptyWishlist.setText(getString(R.string.no_internet_connection));
+        }
     }
+
 }
