bcc6ce9d5e7071566ab2f24e8492d62081a5917b
Marwa-Said <marwa_eltayeb@yahoo.com>
2020-03-30 00:03:51 +0200

    Implement OnNetworkListener on CategoryActivity

--- Souq_Android/app/src/main/java/com/marwaeltayeb/souq/view/CategoryActivity.java
+++ Souq_Android/app/src/main/java/com/marwaeltayeb/souq/view/CategoryActivity.java
@@ -4,10 +4,15 @@ import android.arch.lifecycle.Observer;
 import android.arch.lifecycle.ViewModelProviders;
 import android.arch.paging.PagedList;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.content.res.Configuration;
 import android.databinding.DataBindingUtil;
+import android.net.ConnectivityManager;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.design.widget.Snackbar;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.GridLayoutManager;
@@ -18,17 +23,22 @@ import com.marwaeltayeb.souq.adapter.ProductAdapter;
 import com.marwaeltayeb.souq.databinding.ActivityCategoryBinding;
 import com.marwaeltayeb.souq.model.Product;
 import com.marwaeltayeb.souq.net.ProductDataSourceFactory;
+import com.marwaeltayeb.souq.receiver.NetworkChangeReceiver;
 import com.marwaeltayeb.souq.utils.Constant;
+import com.marwaeltayeb.souq.utils.OnNetworkListener;
 
 import static com.marwaeltayeb.souq.utils.Constant.PRODUCT;
+import static com.marwaeltayeb.souq.utils.InternetUtils.isNetworkConnected;
 
-public class CategoryActivity extends AppCompatActivity implements ProductAdapter.ProductAdapterOnClickHandler{
+public class CategoryActivity extends AppCompatActivity implements ProductAdapter.ProductAdapterOnClickHandler, OnNetworkListener {
 
     private static final String TAG = "CategoryActivity";
     private ActivityCategoryBinding binding;
     private ProductAdapter productAdapter;
     private CategoryViewModel categoryViewModel;
     private String category;
+    private Snackbar snack;
+    private NetworkChangeReceiver mNetworkReceiver;
     
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,6 +50,8 @@ public class CategoryActivity extends AppCompatActivity implements ProductAdapte
             // This line shows Up button
             actionBar.setDisplayHomeAsUpEnabled(true);
         }
+
+        snack = Snackbar.make(findViewById(android.R.id.content), getResources().getString(R.string.no_internet_connection), Snackbar.LENGTH_INDEFINITE);
 
         // Get Category from ProductActivity Intent
         Intent intent = getIntent();
@@ -53,25 +65,30 @@ public class CategoryActivity extends AppCompatActivity implements ProductAdapte
         setupRecyclerViews();
 
         getProductsByCategory();
+
+        mNetworkReceiver = new NetworkChangeReceiver();
+        mNetworkReceiver.setOnNetworkListener(this);
     }
 
     private void setupRecyclerViews() {
-        binding.recyclerView.setLayoutManager(new GridLayoutManager(this, (getResources().getConfiguration().orientation == Configuration.ORIENTATION_PORTRAIT) ? 2 : 4));
-        binding.recyclerView.setHasFixedSize(true);
+        binding.categoryList.setLayoutManager(new GridLayoutManager(this, (getResources().getConfiguration().orientation == Configuration.ORIENTATION_PORTRAIT) ? 2 : 4));
+        binding.categoryList.setHasFixedSize(true);
         productAdapter = new ProductAdapter(this,this);
     }
 
 
     public void getProductsByCategory() {
-        categoryViewModel.categoryPagedList.observe(this, new Observer<PagedList<Product>>() {
-            @Override
-            public void onChanged(@Nullable PagedList<Product> products) {
-                productAdapter.notifyDataSetChanged();
-                productAdapter.submitList(products);
-            }
-        });
+        if (isNetworkConnected(this)) {
+            categoryViewModel.categoryPagedList.observe(this, new Observer<PagedList<Product>>() {
+                @Override
+                public void onChanged(@Nullable PagedList<Product> products) {
+                    productAdapter.notifyDataSetChanged();
+                    productAdapter.submitList(products);
+                }
+            });
 
-        binding.recyclerView.setAdapter(productAdapter);
+            binding.categoryList.setAdapter(productAdapter);
+        }
     }
 
     @Override
@@ -80,5 +97,46 @@ public class CategoryActivity extends AppCompatActivity implements ProductAdapte
         // Pass an object of product class
         intent.putExtra(PRODUCT, (product));
         startActivity(intent);
+    }
+
+    @Override
+    public void onNetworkConnected() {
+        hideSnackBar();
+        getProductsByCategory();
+    }
+
+    @Override
+    public void onNetworkDisconnected() {
+        showSnackBar();
+    }
+
+    public void showSnackBar() {
+        snack.getView().setBackgroundColor(ContextCompat.getColor(this, R.color.red));
+        snack.show();
+    }
+
+    public void hideSnackBar() {
+        snack.dismiss();
+    }
+
+    private void registerNetworkBroadcastForNougat() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+            registerReceiver(mNetworkReceiver, new IntentFilter(ConnectivityManager.CONNECTIVITY_ACTION));
+        }
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            registerReceiver(mNetworkReceiver, new IntentFilter(ConnectivityManager.CONNECTIVITY_ACTION));
+        }
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        registerNetworkBroadcastForNougat();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        unregisterReceiver(mNetworkReceiver);
     }
 }
