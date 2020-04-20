47cdb3332000ee7cee6af4da25e2f2089dc013a5
Marwa-Said <marwa_eltayeb@yahoo.com>
2020-04-20 22:20:34 +0200

    Get Order's status

---
+++ Souq_Android/app/src/main/java/com/marwaeltayeb/souq/view/StatusActivity.java
@@ -0,0 +1,46 @@
+package com.marwaeltayeb.souq.view;
+
+import android.content.Intent;
+import android.databinding.DataBindingUtil;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+import com.marwaeltayeb.souq.R;
+import com.marwaeltayeb.souq.databinding.ActivityStatusBinding;
+import com.marwaeltayeb.souq.model.Order;
+
+import static com.marwaeltayeb.souq.utils.Constant.ORDER;
+
+public class StatusActivity extends AppCompatActivity implements View.OnClickListener {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        ActivityStatusBinding binding = DataBindingUtil.setContentView(this, R.layout.activity_status);
+
+        // Receive the order object
+        Intent intent = getIntent();
+        Order order = (Order) intent.getSerializableExtra(ORDER);
+
+        binding.orderDate.setText(order.getOrderDate());
+        binding.orderNumber.setText(order.getOrderNumber());
+        binding.userName.setText(order.getUserName());
+        binding.userAddress.setText(order.getShippingAddress());
+        binding.userPhone.setText(order.getShippingPhone());
+        binding.txtProductName.setText(order.getProductName());
+        binding.txtProductPrice.setText(String.valueOf(order.getProductPrice()));
+        String status = getString(R.string.item, order.getOrderDateStatus());
+        binding.orderStatus.setText(status);
+
+        binding.reOrder.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        if (view.getId() == R.id.reOrder) {
+            Intent reOrderIntent = new Intent(this, OrderProductActivity.class);
+            startActivity(reOrderIntent);
+        }
+    }
+}
